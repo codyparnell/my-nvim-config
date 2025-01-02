c47062aa45dd27e3be179adf6575a4036940516b
cody parnell <68557392+codyparnell@users.noreply.github.com>
2025-01-02 16:44:57 -0600

    version updates

--- init.lua
+++ init.lua
@@ -28,18 +28,6 @@ vim.cmd([[colorscheme catppuccin]])
 
 vim.opt.breakindent = true
 
-require("nvim-treesitter.configs").setup({
-  ensure_installed = { "javascript", "markdown", "typescript" }, -- Install all maintained parsers
-  sync_install = true, -- Install parsers synchronously (only applied to `ensure_installed`)
-  ignore_install = {}, -- List of parsers to ignore installing
-  auto_install = true, -- Automatically install missing parsers when entering buffer
-  highlight = {
-    enable = true, -- Enable for all filetypes
-    disable = { "plaintext", "text", "gitattributes" }, -- Disable for these filetypes
-  },
-  modules = {},
-})
-
 local cmp = require("cmp")
 
 -- Get the existing configuration
